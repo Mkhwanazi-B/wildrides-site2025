b1c20878ddf101db1ba6a4540727454ad3d124e6
Mkhwanazi-B <blessing67mkhwanazi@gmail.com>
2025-05-23 14:08:46 +0200

    Update config.js (cognito auth)

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // eu-north-1_HWqXfWCCO
-        userPoolClientId: '', // 3epqkdgvcbo7ebb2gjf0kp1nvf
-        region: '' // eu-north-1
+        userPoolId: 'eu-north-1_HWqXfWCCO',
+        userPoolClientId: '3epqkdgvcbo7ebb2gjf0kp1nvf',
+        region: 'eu-north-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
